33b367a1830ec3da3ff08229f2aa81623baacfa4
MrJosh <not_thorrox@web.de>
2022-07-03 18:32:05 +0200

    Initial commit

--- js/countdown.js
+++ js/countdown.js
@@ -11,18 +11,39 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor(distance / (1000 * 60 * 60));
-  var minutes = Math.floor(distance / (1000 * 60));
-  var seconds = Math.floor(distance / 1000);
+  if (((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) > 12){
+    var days = Math.floor((distance / (1000 * 60 * 60 * 24)) + 1);
+  } else {
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  }
+
+  if (((distance % (1000 * 60 * 60)) / (1000 * 60)) > 30){
+    var hours = Math.floor((distance / (1000 * 60 * 60)) + 1);
+  } else {
+    var hours = Math.floor(distance / (1000 * 60 * 60));
+  }
+  
+  if (((distance % (1000 * 60)) / 1000) > 30){
+    var minutes = Math.floor((distance / (1000 * 60)) + 1);
+  } else {
+    var minutes = Math.floor(distance / (1000 * 60));
+  }
+  
+  if ((distance % (1000)) > 500){
+    var seconds = Math.floor((distance / 1000) + 1);
+  } else {
+    var seconds = Math.floor(distance / 1000);
+  }
+  
+  var miliseconds = distance;
 
   // Display the result in the element with id="demo"
   document.getElementById("cdtm").innerHTML = days + "d<br>" + hours + "h<br>"
-  + minutes + "m<br>" + seconds + "s";
+  + minutes + "m<br>" + seconds + "s<br>" + miliseconds + "ms";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("cdtm").innerHTML = "EXPIRED";
+    document.getElementById("cdtm").innerHTML = "Entweder bist du zu spät,<br>oder nicht aufmerksam";
   }
-}, 1000);
+}, 1);
