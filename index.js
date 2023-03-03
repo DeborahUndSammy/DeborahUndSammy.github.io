0fd459f4c9389e2a96540beab9e75607af389cb3
MrJ0sh <josua.zinser@gmail.com>
2023-03-03 09:55:55 +0100

    Initial Commit

--- index.js
+++
@@ -1,3 +0,0 @@
-var hochzeitsdatum = new Date("Sep 2, 2023 13:00:00").getTime() / 1000;
-
-var flipdown = new FlipDown(hochzeitsdatum, {theme: "dark"}).start();
---
+++ js/index.js
@@ -0,0 +1,3 @@
+var hochzeitsdatum = new Date("Sep 2, 2023 13:00:00").getTime() / 1000;
+
+var flipdown = new FlipDown(hochzeitsdatum, {theme: "dark"}).start();
