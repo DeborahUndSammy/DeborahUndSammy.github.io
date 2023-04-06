ddff47956d73461b852e9d9e0c0bea09044ce01f
MrJ0sh <josua.zinser@gmail.com>
2023-04-06 23:59:04 +0200

    Initial Commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
-var hochzeitsdatum = new Date("Sep 2, 2023 14:00:00").getTime() / 1000;
+
+var hochzeitsdatum = new Date("Sep 2, 2023 13:30:00").getTime() / 1000;
 
 var flipdown = new FlipDown(hochzeitsdatum, {theme: "dark"}).start();
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    
+    
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
